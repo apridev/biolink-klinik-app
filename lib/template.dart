a203d95a083f7e7b00a71b55e447bc3893a1d33b
Apridev <apriantofajar11@gmail.com>
2022-04-18 23:16:25 +0900

    template app

---
+++ lib/template.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = Color(0xff5ECF78);
+Color secondaryColor = Color(0xff00B4D8);
+Color backgroundColor = Color(0xffffffff);
+Color textPrimaryColor = Color(0xff393E46);
+Color textSubtitleLoginColor = Color(0xff7D7D7D);
+Color inputFormColor = Color(0xff7D7D7D);
+Color inputTextColor = Color(0xff979797);
+Color inputCodeOTP = Color(0xffEBEBEB);
+
+TextStyle primaryTextStyle = GoogleFonts.kanit(
+  color: primaryColor
+);
+
+TextStyle secondaryTextColor = GoogleFonts.kanit(
+  color: secondaryColor
+);
+
+TextStyle textSubtitleLogin = GoogleFonts.kanit(
+  color: textSubtitleLoginColor
+);
+
+TextStyle textPrimaryStyle = GoogleFonts.kanit(
+  color: textPrimaryColor
+);
+
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
